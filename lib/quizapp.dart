87b48890c7b7e1337608aec5257176ba74aa7621
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-07 14:28:04 +0500

    update

--- lib/quizapp.dart
+++ lib/quizapp.dart
@@ -1,24 +1,24 @@
 import 'dart:io';
 
-dynamic input(String type, String text) {
+dynamic input(String type, [String? text]) {
   switch (type) {
     case 'i' || 'I':
       try {
-        print(text);
+        print(text ?? 'Enter:');
         return int.parse(stdin.readLineSync()!);
       } catch (e) {
         print('The value you entered is not int.');
       }
     case 's' || 'S':
       try {
-        print(text);
+        print(text ?? 'Enter:');
         return stdin.readLineSync()!;
       } catch (e) {
         print('The value you entered is not String.');
       }
     case 'd' || 'D':
       try {
-        print(text);
+        print(text ?? 'Enter:');
         return double.parse(stdin.readLineSync()!);
       } catch (e) {
         print('The value you entered is not double.');
@@ -30,12 +30,13 @@ dynamic input(String type, String text) {
 
 class Questionformat {
   List<String>? question;
-  List<String>? ans;
+  List<String> ans = [];
   Questionformat(this.question);
 
   void dispayQuestoins() {
     for (String que in question!) {
       print(que);
+      ans.add(input('i' , 'Select any one option A , B, C, D'));
     }
   }
 }
