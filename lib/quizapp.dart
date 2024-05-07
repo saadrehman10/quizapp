fd95fa9e3c393ec480622b90ed899750a3b55649
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-07 14:20:03 +0500

    update

--- lib/quizapp.dart
+++ lib/quizapp.dart
@@ -1,6 +1,31 @@
-abstract class Questionformat {
-  String? question;
-  
+import 'dart:io';
+
+dynamic input(String type, String text) {
+  switch (type) {
+    case 'i' || 'I':
+      try {
+        return int.parse(stdin.readLineSync()!);
+      } catch (e) {
+        print('The value you entered is not int.');
+      }
+    case 's' || 'S':
+      try {
+        return int.parse(stdin.readLineSync()!);
+      } catch (e) {
+        print('The value you entered is not String.');
+      }
+ 
+  }
 }
 
+class Questionformat {
+  List<String>? question;
+  List<String>? ans;
+  Questionformat(this.question);
 
+  void dispayQuestoins() {
+    for (String que in question!) {
+      print(que);
+    }
+  }
+}
--- test/quizapp_test.dart
+++ test/quizapp_test.dart
@@ -1,8 +1,6 @@
-import 'package:quizapp/quizapp.dart';
-import 'package:test/test.dart';
+// import 'package:quizapp/quizapp.dart';
+// import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  
 }
