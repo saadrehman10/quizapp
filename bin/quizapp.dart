e5b9ed1b89472d1980c1e191d6293a14051d0287
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-07 14:12:30 +0500

    update

--- bin/quizapp.dart
+++ bin/quizapp.dart
@@ -1,5 +1,5 @@
 import 'package:quizapp/quizapp.dart' as quizapp;
 
 void main(List<String> arguments) {
-  print('Hello world: ${quizapp.calculate()}!');
+  
 }
--- lib/quizapp.dart
+++ lib/quizapp.dart
@@ -1,3 +1,6 @@
-int calculate() {
-  return 6 * 7;
+abstract class Questionformat {
+  String? question;
+  
 }
+
+
