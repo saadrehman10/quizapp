046d86e7f2fc8351324f21f8fd4aec28168f897e
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-08 08:30:35 +0500

    update

--- lib/filehandling.dart
+++ lib/filehandling.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 String fileHanding() {
-  File file = File('../assets/question.cvs');
+  File file = File('C:/Users/saadr/Desktop/quizapp/assets/question.cvs');
   String content = file.readAsStringSync();
   return content;
 }
