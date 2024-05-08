1b98f9d93c3aa81731d73867c38bda45f54058ee
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-08 19:47:24 +0500

    changes

--- lib/quizapp.dart
+++ lib/quizapp.dart
@@ -1,22 +1,23 @@
 import 'dart:io';
 
-dynamic input(String type, [String? text]) {
-  switch (type) {
-    case 'i' || 'I':
+class Input<T> {
+  T input([String? text]) {
+  switch (T) {
+    case  int :
       try {
         print(text ?? 'Enter:');
         return int.parse(stdin.readLineSync()!);
       } catch (e) {
         print('The value you entered is not int.');
       }
-    case 's' || 'S':
+    case String:
       try {
         print(text ?? 'Enter:');
         return stdin.readLineSync()!;
       } catch (e) {
         print('The value you entered is not String.');
       }
-    case 'd' || 'D':
+    case double:
       try {
         print(text ?? 'Enter:');
         return double.parse(stdin.readLineSync()!);
@@ -24,11 +25,15 @@ dynamic input(String type, [String? text]) {
         print('The value you entered is not double.');
       }
     default:
-      print('please define data type');
+      print('please define data type')
+      return null;
   }
+ }
 }
 
-class Questionformat {
+
+
+class Questionformat  extends Input{
   Set<String>? question;
   List<String> userAns = [];
   Questionformat(this.question);
@@ -39,7 +44,7 @@ class Questionformat {
       bool temp = true;
       while (temp) {
         var option = 'aAbBcCdD'.split('');
-        var ans = input('s', 'Select one option: ');
+        var ans = super.input<int>('Select one option: ');
         if (ans.length == 1 && option.contains(ans)) {
           userAns.add(ans);
           temp = false;
