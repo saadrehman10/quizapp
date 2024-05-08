13db5728476fedecc26577a7b9bb8ced67e22898
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-08 19:54:24 +0500

    changes

--- lib/quizapp.dart
+++ lib/quizapp.dart
@@ -1,34 +1,44 @@
 import 'dart:io';
 
 class Input<T> {
-  T input([String? text]) {
-  switch (T) {
-    case  int :
-      try {
-        print(text ?? 'Enter:');
-        return int.parse(stdin.readLineSync()!);
-      } catch (e) {
-        print('The value you entered is not int.');
-      }
-    case String:
-      try {
-        print(text ?? 'Enter:');
-        return stdin.readLineSync()!;
-      } catch (e) {
-        print('The value you entered is not String.');
-      }
-    case double:
-      try {
-        print(text ?? 'Enter:');
-        return double.parse(stdin.readLineSync()!);
-      } catch (e) {
-        print('The value you entered is not double.');
-      }
-    default:
-      print('please define data type')
-      return null;
+  // dynamic input([String? text]) {
+  // switch (T) {
+  //   case  int :
+  //     try {
+  //       print(text ?? 'Enter:');
+  //       return T.parse(stdin.readLineSync()!);
+  //     } catch (e) {
+  //       print('The value you entered is not int.');
+  //     }
+  //   case String:
+  //     try {
+  //       print(text ?? 'Enter:');
+  //       return stdin.readLineSync()!;
+  //     } catch (e) {
+  //       print('The value you entered is not String.');
+  //     }
+  //   case double:
+  //     try {
+  //       print(text ?? 'Enter:');
+  //       return double.parse(stdin.readLineSync()!);
+  //     } catch (e) {
+  //       print('The value you entered is not double.');
+  //     }
+  //   default:
+  //     print('please define data type')
+  //     return null;
+  // }
+//  }
+  T? input([String? text]){
+    try {
+      print(text ?? 'Enter:');
+      return T.parse(stdin.readLineSync()!);
+    } catch (e) {
+      print('The value you entered is not $T.');
+    }
   }
- }
+    
+
 }
 
 
@@ -44,7 +54,7 @@ class Questionformat  extends Input{
       bool temp = true;
       while (temp) {
         var option = 'aAbBcCdD'.split('');
-        var ans = super.input<int>('Select one option: ');
+        var ans = input<int>('Select one option: ');
         if (ans.length == 1 && option.contains(ans)) {
           userAns.add(ans);
           temp = false;
