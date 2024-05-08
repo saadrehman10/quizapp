91b55234d8fda0a4da375ce8ff7d8ee6e23c92b0
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-08 20:42:40 +0500

    changes

--- lib/quizapp.dart
+++ lib/quizapp.dart
@@ -1,22 +1,18 @@
 import 'dart:io';
 
 class Input {
-   input<T>([String? text]) {
+  dynamic input<T>([String? text]) {
     try {
       print(text ?? 'Enter:');
-      final inputString = stdin.readLineSync();
-      if (inputString != null) {
-        if (T == String) {
-          return inputString as T;
-        } else if (T == int) {
-          return int.parse(inputString) as T;
-        } else if (T == double) {
-          return double.parse(inputString) as T;
-        }
-        // Add more type checks for other supported types if needed
-      } else {
-        print('No input provided.');
+      final String inputString = stdin.readLineSync()!;
+      if (T == String) {
+        return inputString;
+      } else if (T == int) {
+        return int.parse(inputString) as T;
+      } else if (T == double) {
+        return double.parse(inputString) as T;
       }
+      // Add more type checks for other supported types if needed
     } catch (e) {
       print('The value you entered is not of type $T.');
     }
